7ea6481d944b3ced41b8f6cbfe773b59f2f48b09
Ricardo Leyva <ricardoandres9728@hotmail.com>
2025-09-24 20:35:18 -0500

    "Initial commit for RestClient project with basic setup, including configuration files, service extensions, solution structure, and dependencies."

--- Extensions/ServiceCollectionExtensions.cs
+++ Extensions/ServiceCollectionExtensions.cs
@@ -1,37 +1,12 @@
-using ApimIdenty.Options;
-using ApimIdenty.Services;
-using Azure.Core;
 using Azure.Identity;
+using NameProject.RestClient;
+using NameProject.RestClient.Common;
+using NameProject.RestClient.Configurations;
 
-
-namespace ApimIdenty.Extensions;
+namespace RestClient.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
-
-        services.AddOptions<ApimOptions>()
-            .Bind(configuration.GetSection("Apim"))
-            .ValidateDataAnnotations()
-            .Validate(
-                options => Uri.TryCreate(options.BaseUrl, UriKind.Absolute, out _),
-                "Apim:BaseUrl configuration must be a valid absolute URI.")
-            .Validate(
-                options => !string.IsNullOrWhiteSpace(options.Scope),
-                "Apim:Scope configuration is required.");
-
-        services.AddSingleton<TokenCredential, DefaultAzureCredential>();
-        services.AddTransient<BearerTokenHandler>();
-
-        services.AddHttpClient<ApimClient>()
-            .AddHttpMessageHandler<BearerTokenHandler>();
-
-        return services;
-    }
-
     public static IServiceCollection AddHttpClientConfiguration(this IServiceCollection services)
     {
         var serviceConfiguration = GetServiceConfiguration(services);
@@ -69,8 +44,7 @@ public static class ServiceCollectionExtensions
                 TokenRequest = setting
             });
         });
-
-        //services.AddTransient<IClienteinformacionServices, ClienteinformacionServices>();
+        
 
         return services;
     }
